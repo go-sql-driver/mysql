36cccb24339c6453a39018018113a0b23d588730
Julien Schmidt <github@julienschmidt.com>
2013-09-15 15:11:23 +0200

    transactions: check if the netConn ist valid

--- transaction.go
+++ transaction.go
@@ -14,7 +14,7 @@ type mysqlTx struct {
 }
 
 func (tx *mysqlTx) Commit() (err error) {
-	if tx.mc == nil {
+	if tx.mc == nil || tx.mc.netConn == nil {
 		return errInvalidConn
 	}
 	err = tx.mc.exec("COMMIT")
@@ -23,7 +23,7 @@ func (tx *mysqlTx) Commit() (err error) {
 }
 
 func (tx *mysqlTx) Rollback() (err error) {
-	if tx.mc == nil {
+	if tx.mc == nil || tx.mc.netConn == nil {
 		return errInvalidConn
 	}
 	err = tx.mc.exec("ROLLBACK")
