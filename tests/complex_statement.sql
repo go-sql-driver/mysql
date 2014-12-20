12dca5e4f02c4ff51f0d3b4161a1ce1f426485c1
David Francoeur <dfrancoeur04@gmail.com>
2014-12-19 19:14:35 -0500

    Added execution of multiple statement at once with two tests An external file was created to avoid crap in the go test file

---
+++ tests/complex_statement.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS `test` (
+	  `id` int(11),
+	  `value` int(11),
+	  PRIMARY KEY (`id`)
+	) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+INSERT INTO test VALUES(1,1);
+INSERT INTO test VALUES(2,1);
