88eea426ec9ad49b0e8b5afab4fb077d11688fef
INADA Naoki <songofacandy@gmail.com>
2016-10-20 17:18:52 +0900

    fixup

--- connection_test.go
+++ connection_test.go
@@ -1,6 +1,6 @@
 // Go MySQL Driver - A MySQL-Driver for Go's database/sql package
 //
-// Copyright 2013 The Go-MySQL-Driver Authors. All rights reserved.
+// Copyright 2016 The Go-MySQL-Driver Authors. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
@@ -43,6 +43,22 @@ func TestInterpolateParamsTooManyPlaceholders(t *testing.T) {
 	}
 
 	q, err := mc.interpolateParams("SELECT ?+?", []driver.Value{int64(42)})
+	if err != driver.ErrSkip {
+		t.Errorf("Expected err=driver.ErrSkip, got err=%#v, q=%#v", err, q)
+	}
+}
+
+// We don't support placeholder in string literal for now.
+func TestInterpolateParamsPlaceholderInString(t *testing.T) {
+	mc := &mysqlConn{
+		buf:              newBuffer(nil),
+		maxPacketAllowed: maxPacketSize,
+		cfg: &Config{
+			InterpolateParams: true,
+		},
+	}
+
+	q, err := mc.interpolateParams("SELECT 'abc?xyz',?", []driver.Value{int64(42)})
 	if err != driver.ErrSkip {
 		t.Errorf("Expected err=driver.ErrSkip, got err=%#v, q=%#v", err, q)
 	}
