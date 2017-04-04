6b890cc94f738cbbfeacfb7e654384209e580142
Evan Shaw <evan@vendhq.com>
2017-04-04 14:48:33 +1200

    Ping reads its result; obeys the context's Done channel

--- connection_go18.go
+++ connection_go18.go
@@ -7,5 +7,18 @@ import (
 )
 
 func (mc *mysqlConn) Ping(ctx context.Context) error {
-	return mc.writeCommandPacket(comPing)
+	err := mc.writeCommandPacket(comPing)
+	if err != nil {
+		return err
+	}
+
+	select {
+	case <-ctx.Done():
+		mc.Close()
+		return ctx.Err()
+	default:
+	}
+
+	_, err = mc.readResultOK()
+	return err
 }
