32d1a5fb0bb2229c62740ec9031c868d9b4030a4
Julien Schmidt <git@julienschmidt.com>
2017-05-06 17:40:42 +0800

    fields: fix copyright year

--- fields.go
+++ fields.go
@@ -1,6 +1,6 @@
 // Go MySQL Driver - A MySQL-Driver for Go's database/sql package
 //
-// Copyright 2012 The Go-MySQL-Driver Authors. All rights reserved.
+// Copyright 2017 The Go-MySQL-Driver Authors. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
