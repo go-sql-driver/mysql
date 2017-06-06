31a72667281cf440bed9aeb9c06d348edf0c51e6
Ichinose Shogo <shogo82148@gmail.com>
2017-06-06 15:06:56 +0900

    move namedValueToValue to utils_go18.go

--- connection_go18.go
+++ connection_go18.go
@@ -203,14 +203,3 @@ func (mc *mysqlConn) startWatcher() {
 		}
 	}()
 }
-
-func namedValueToValue(named []driver.NamedValue) ([]driver.Value, error) {
-	dargs := make([]driver.Value, len(named))
-	for n, param := range named {
-		if len(param.Name) > 0 {
-			return nil, errors.New("mysql: driver does not support the use of Named Parameters")
-		}
-		dargs[n] = param.Value
-	}
-	return dargs, nil
-}
