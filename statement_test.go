db6af9049fa84406c76edaf472a6b50f7eff1cc1
Daniel Montoya <dsmontoyam@gmail.com>
2017-11-12 23:03:08 -0600

    Added a newline to force a commit.

--- statement_test.go
+++ statement_test.go
@@ -15,6 +15,7 @@ type customString string
 func TestConvertValueCustomTypes(t *testing.T) {
 	var cstr customString = "string"
 	c := converter{}
+  
 	if _, err := c.ConvertValue(cstr); err != nil {
 		t.Errorf("custom string type should be valid")
 	}
