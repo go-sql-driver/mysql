9292b24955e39639be69c5fb5242cc7b2c2367e6
Daniel Montoya <dsmontoyam@gmail.com>
2017-11-15 16:25:32 -0600

    Remove newline.

--- statement_test.go
+++ statement_test.go
@@ -15,7 +15,6 @@ type customString string
 func TestConvertValueCustomTypes(t *testing.T) {
 	var cstr customString = "string"
 	c := converter{}
-  
 	if _, err := c.ConvertValue(cstr); err != nil {
 		t.Errorf("custom string type should be valid")
 	}
