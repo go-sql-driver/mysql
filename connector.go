ccd26221cb6abfc712c15d479695faa8684b7b61
Ichinose Shogo <shogo82148@gmail.com>
2018-04-07 15:48:34 +0900

    no need to check the driver implements the watcher interface.
    because Connector.Connect is built with Go1.10 or higher.

--- connector.go
+++ connector.go
@@ -58,9 +58,7 @@ func (c Connector) Connect(ctx context.Context) (driver.Conn, error) {
 	}
 
 	// Call startWatcher for context support (From Go 1.8)
-	if s, ok := interface{}(mc).(watcher); ok {
-		s.startWatcher()
-	}
+	mc.startWatcher()
 	if err := mc.watchCancel(ctx); err != nil {
 		return nil, err
 	}
