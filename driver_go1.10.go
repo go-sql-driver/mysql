72d9da99543da81dfcc1a2a0b7f32ce45d6f41a7
Ichinose Shogo <shogo82148@gmail.com>
2018-04-07 15:38:14 +0900

    Implement Connector and DriverContext interface

---
+++ driver_go1.10.go
@@ -0,0 +1,45 @@
+// Go MySQL Driver - A MySQL-Driver for Go's database/sql package
+//
+// Copyright 2018 The Go-MySQL-Driver Authors. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// You can obtain one at http://mozilla.org/MPL/2.0/.
+
+// +build go1.10
+
+package mysql
+
+import (
+	"crypto/rsa"
+	"database/sql/driver"
+	"math/big"
+)
+
+// NewConnector returns new driver.Connector.
+func NewConnector(cfg *Config) driver.Connector {
+	copyCfg := *cfg
+	copyCfg.tls = cfg.tls.Clone()
+	copyCfg.Params = make(map[string]string, len(cfg.Params))
+	for k, v := range cfg.Params {
+		copyCfg.Params[k] = v
+	}
+	if cfg.pubKey != nil {
+		copyCfg.pubKey = &rsa.PublicKey{
+			N: new(big.Int).Set(cfg.pubKey.N),
+			E: cfg.pubKey.E,
+		}
+	}
+	return &connector{cfg: &copyCfg}
+}
+
+// OpenConnector implements driver.DriverContext.
+func (d MySQLDriver) OpenConnector(dsn string) (driver.Connector, error) {
+	cfg, err := ParseDSN(dsn)
+	if err != nil {
+		return nil, err
+	}
+	return &connector{
+		cfg: cfg,
+	}, nil
+}
