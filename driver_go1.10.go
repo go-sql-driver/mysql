cfaf043ea7a44f589a7501ccbc267187b312291b
Ichinose Shogo <shogo82148@gmail.com>
2018-06-16 19:15:52 +0900

    rename driver_go110.go

--- driver_go1.10.go
+++ driver_go1.10.go
@@ -1,12 +1,10 @@
 // Go MySQL Driver - A MySQL-Driver for Go's database/sql package
 //
-// Copyright 2012 The Go-MySQL-Driver Authors. All rights reserved.
+// Copyright 2018 The Go-MySQL-Driver Authors. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
-
-// +build go1.10
 
 package mysql
 
