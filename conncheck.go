ceed4d4ae3f7df17c73bdffa282c0043d047946b
Ichinose Shogo <shogo82148@gmail.com>
2020-01-05 13:13:50 +0900

    fix compile error of connCheck.go

--- conncheck.go
+++ conncheck.go
@@ -6,7 +6,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// +build !windows,!appengine
+// +build linux darwin
+// +build !appengine
 
 package mysql
 
