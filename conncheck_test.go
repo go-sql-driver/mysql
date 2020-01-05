87239404061139bfbd9a61b167c5ed54539e6086
Ichinose Shogo <shogo82148@gmail.com>
2020-01-05 05:18:52 +0000

    fix compile error of connCheck.go (#1048)
    * fix compile error of connCheck.go
    
    * remove "appengine" build tag

--- conncheck_test.go
+++ conncheck_test.go
@@ -6,7 +6,7 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this file,
 // You can obtain one at http://mozilla.org/MPL/2.0/.
 
-// +build go1.10,!windows
+// +build linux darwin
 
 package mysql
 
