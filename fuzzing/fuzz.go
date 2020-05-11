a46920bcb12ac5de235b8d93459f79e4bae034af
Adam Korczynski <adam@adalogics.com>
2020-05-11 15:24:55 +0100

    Added fuzzer

---
+++ fuzzing/fuzz.go
@@ -0,0 +1,13 @@
+package fuzz
+
+import "database/sql"
+import _ "github.com/go-sql-driver/mysql"
+
+func Fuzz(data []byte) int {
+	db, err := sql.Open("mysql", string(data))
+	if err != nil {
+		return 0
+	}
+	defer db.Close()
+	return 1
+}
