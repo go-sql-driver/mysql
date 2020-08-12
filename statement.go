3892d84b83bdefd14a0d13e116718db63e00bd75
Sho Ikeda <suicaicoca@gmail.com>
2020-08-13 01:02:31 +0900

    Support returning uint64 from Valuer in ConvertValue
    https://golang.org/pkg/database/sql/driver/#Value says:
    
    > Value is a value that drivers must be able to handle. It is either nil, a type handled by a database driver's NamedValueChecker interface, or an instance of one of these types:

--- statement.go
+++ statement.go
@@ -154,10 +154,16 @@ func (c converter) ConvertValue(v interface{}) (driver.Value, error) {
 		if err != nil {
 			return nil, err
 		}
-		if !driver.IsValue(sv) {
-			return nil, fmt.Errorf("non-Value type %T returned from Value", sv)
+		if driver.IsValue(sv) {
+			return sv, nil
 		}
-		return sv, nil
+		// A value returend from the Valuer interface can be "a type handled by
+		// a database driver's NamedValueChecker interface" so we should accept
+		// uint64 here as well.
+		if u, ok := sv.(uint64); ok {
+			return u, nil
+		}
+		return nil, fmt.Errorf("non-Value type %T returned from Value", sv)
 	}
 	rv := reflect.ValueOf(v)
 	switch rv.Kind() {
