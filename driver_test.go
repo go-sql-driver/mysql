35f1118c8f0bb91ccce655694d875c7f0f5a5217
Julien Schmidt <git@julienschmidt.com>
2020-08-14 19:41:28 +0200

    infile: reword errors

--- driver_test.go
+++ driver_test.go
@@ -1356,7 +1356,7 @@ func TestLoadData(t *testing.T) {
 		_, err = dbt.db.Exec("LOAD DATA LOCAL INFILE 'Reader::doesnotexist' INTO TABLE test")
 		if err == nil {
 			dbt.Fatal("load non-existent Reader didn't fail")
-		} else if err.Error() != "Reader 'doesnotexist' is not registered" {
+		} else if err.Error() != "unknown Reader 'doesnotexist'" {
 			dbt.Fatal(err.Error())
 		}
 	})
