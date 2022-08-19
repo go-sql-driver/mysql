a477f69f3c2abaf4646680bdc3a65d5172a6566e
ICHINOSE Shogo <shogo82148@gmail.com>
2022-08-20 00:19:54 +0900

    fix: benchmarkExecContext is unused (#1351)

--- benchmark_test.go
+++ benchmark_test.go
@@ -314,7 +314,7 @@ func BenchmarkExecContext(b *testing.B) {
 	defer db.Close()
 	for _, p := range []int{1, 2, 3, 4} {
 		b.Run(fmt.Sprintf("%d", p), func(b *testing.B) {
-			benchmarkQueryContext(b, db, p)
+			benchmarkExecContext(b, db, p)
 		})
 	}
 }
